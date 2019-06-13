bf1f688904fada018ce3b5687e7d21f874b917ae
LFuryk <leandro.furyk@mercadolibre.com>
2019-06-13 20:18:01 -0300

    main activity

--- app/src/main/java/com/example/infoapp/MainActivity.java
+++ app/src/main/java/com/example/infoapp/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.infoapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button newFormButton;
+    private Button saveDataButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        newFormButton = findViewById(R.id.new_form_button);
+        saveDataButton = findViewById(R.id.save_data_button);
+
+        this.configurateNewFormButton();
+        this.configurateSaveDataButton();
+
+    }
+
+    private void configurateNewFormButton(){
+        newFormButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), FormActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void configurateSaveDataButton(){
+        saveDataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Nothing still
+            }
+        });
     }
 
 }
